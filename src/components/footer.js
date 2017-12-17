84d31a4073f71ae71b1c9a590e657a9f2da93c94
Ian Wessen <ian@honeybook.com>
2017-12-17 14:27:36 -0800

    css move

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,21 +10,9 @@ const Footer = () => (
     <section className="footer">
         <div className="container">
             <span className="footer__logo">
-                <h6>🍺🍞</h6>
-                <span>“Does butter have carbs?”</span>
-                <h6>—Regina George</h6>
+                <span>Does butter have carbs?</span>
+                <h6>Regina George</h6>
             </span>
-            <div className="footer__navigation">
-                <Link to="/about/" className="footer__navigation-item">
-                    About
-                </Link>
-                <Link to="/contact/" className="footer__navigation-item">
-                    Contact
-                </Link>
-                <Link to="/archive/" className="footer__navigation-item">
-                    Archive
-                </Link>
-            </div>
         </div>
     </section>
 )
